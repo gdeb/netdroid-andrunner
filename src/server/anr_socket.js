53dc92580469bfc271a52b197a004c8643cc321c
Gery Debongnie <gery.debongnie@gmail.com>
2014-06-08 09:27:42 +0200

    add tests for anr_socket

--- src/server/anr_socket.js
+++ src/server/anr_socket.js
@@ -1,17 +1,18 @@
 /*jslint node: true */
 'use strict';
 
-let logger = require('../common/logger.js'),
+let terminal_logger = require('../common/logger.js'),
     utils = require('../common/utils.js');
 
 //-----------------------------------------------------------------------------
 class ANRSocket {
-    constructor (socket) {
+    constructor (socket, logger) {
         this.socket = socket;
         this.id = utils.uniqueId();
-        logger.info(`New web socket open, with id ${this.id}`);
-        socket.on('close', () => logger.info(`Connection closed: socket ${this.id}`));
-        socket.on('message', msg => logger.info(`[${this.id}, ▲] ${msg}`));
+        this.logger = logger || terminal_logger;
+        this.logger.info(`New web socket open, with id ${this.id}`);
+        socket.on('close', () => this.logger.info(`Connection closed: socket ${this.id}`));
+        socket.on('message', msg => this.logger.info(`[${this.id}, ▲] ${msg}`));
     }
     write (msg) {
         let msg_out = JSON.stringify({
@@ -20,7 +21,7 @@ class ANRSocket {
             content: msg.content,
             answer: msg.answer,
         });
-        logger.info(`[${this.id}, ▼] ${msg_out}`);
+        this.logger.info(`[${this.id}, ▼] ${msg_out}`);
         this.socket.send(msg_out);
     }
     send (type, content, answered_msg) {
---
+++ tests/server/anr_socket.js
@@ -0,0 +1,35 @@
+/*jslint node: true */
+'use strict';
+
+var expect = require('expect.js');
+
+var traceur = require('traceur');
+traceur.require.makeDefault();
+traceur.options.blockBinding = true;
+
+var ANRSocket = require('../../src/server/anr_socket.js');
+
+var EventEmitter = require('events').EventEmitter;
+
+//-----------------------------------------------------------------------------
+var mock_logger = {info: function () {}};
+
+var mock_socket = new EventEmitter();
+mock_socket.send = function (msg) {
+    var result = JSON.parse(msg);
+    expect(result.type).to.be('type');
+    expect(result.content).to.be('content');
+};
+
+//-----------------------------------------------------------------------------
+describe('ANRSocket', function () {
+    it('should write messages', function () {
+        var anr_socket = new ANRSocket(mock_socket, mock_logger);
+        anr_socket.write({type:'type', content:'content'});
+    });
+
+    it('should send messages', function () {
+        var anr_socket = new ANRSocket(mock_socket, mock_logger);
+        anr_socket.send("type", "content");
+    });
+});
