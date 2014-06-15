4ef3463a9a4184f9468fa95c5d186e46ee2e1b80
Gery Debongnie <gery.debongnie@gmail.com>
2014-06-15 21:39:53 +0200

    lots of improvement to access management

--- src/server/index.js
+++ src/server/index.js
@@ -10,61 +10,71 @@ var express = require('express'),
     morgan = require('morgan'),
     Mustache = require('mustache');
 
+//-----------------------------------------------------------------------------
+var port = process.env.PORT || 8080,
+	app = express();
 
-var port = process.env.PORT || 8080;
-
-var app = express();
-
+//-----------------------------------------------------------------------------
 app.engine('html', consolidate.mustache);
 app.set('view engine', 'html');
 app.set('views', './views');
 
-
+//-----------------------------------------------------------------------------
+// Middlewares
 app.use(morgan('short'));
+
+app.use(function ignoreFavicon (req, res, next) {
+	if (req.url === '/favicon.ico') 
+		res.send(404);
+	else
+		next();
+});
+
 app.use(express.static('.tmp/', { maxAge: '99999'})); 
 app.use(cookieParser('TopSecret'));
 app.use(bodyParser());
 app.use(session());
 
-function restrict (req, res, next) {
-	if (!req.session.user) {
-		req.session.error = "Access denied";
-		res.redirect('login');
-	} else {
-		next();
-	}
+app.use(function restrictAccess(req, res, next) {
+	if (req.url === '/' || req.url === '/login' || req.session.user) 
+		return next();
+	req.session.error = "Access denied";
+	res.redirect('login');
+});
+
+//-----------------------------------------------------------------------------
+function render_view(res, view, session) {
+	res.render(view, {
+		error: session.error,
+		success: session.success,
+		username: session.username,
+		partials: {
+			header: 'header',
+			navbar: 'navbar',
+			footer: 'footer',
+		},
+	});
+	delete session.error;
+	delete session.success;
 }
 
-function get_partials() {
-    return {
-        header: 'header',
-        footer: 'footer',
-        navbar: 'navbar',    
-    };
-};
 
+//-----------------------------------------------------------------------------
+// Routes
+//-----------------------------------------------------------------------------
 app.get('/', function(req, res) {
-    res.render('index', {
-    	error: req.session.error,
-        login: true,
-        partials: get_partials()
-    });
+	render_view(res, 'index', req.session);
 });
 
 app.get('/login', function (req, res) {
-	var error = req.session.error;
-	logger.debug(error);
-	delete req.session.error;
-    res.render('login', {
-    	error: error,
-        partials: get_partials()
-    });
+	render_view(res, 'login', req.session);
 });
 
 app.post('/login', function (req, res) {
 	if (req.body.username === 'gery' || req.body.password === 'gery') {
 		req.session.regenerate(function () {
 			req.session.user = 'gery';
+			req.session.success = 'Success';
 			res.redirect('lobby');
 		});
 	} else {
@@ -73,11 +83,8 @@ app.post('/login', function (req, res) {
 	}
 });
 
-app.get('/lobby', restrict, function (req, res) {
-    res.render('lobby', {
-    	error: req.session.error,
-        partials: get_partials()
-    });
+app.get('/lobby', function (req, res) {
+	render_view(res, 'lobby', req.session);
 });
 
 
