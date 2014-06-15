fe6a32aecbef09ce5acdda153dbcc43e4bbf3ffe
Gery Debongnie <gery.debongnie@gmail.com>
2014-06-15 17:51:06 +0200

    refactoring in progress

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,10 @@
 var express = require('express'),
     logger = require('../logger'),
     consolidate = require('consolidate'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    session = require('express-session'),
+    morgan = require('morgan'),
     Mustache = require('mustache');
 
 
@@ -16,7 +20,28 @@ app.set('view engine', 'html');
 app.set('views', './views');
 
 
-app.use(express.static('.tmp/')); 
+app.use(morgan('short'));
+app.use(express.static('.tmp/', { maxAge: '99999'})); 
+app.use(cookieParser('TopSecret'));
+app.use(bodyParser());
+app.use(session());
+app.use(function (req, res, next) {
+	if (req.session.last) {
+		logger.debug('Last page:' + req.session.last);
+	}
+	req.session.last = req.url;
+	next();
+});
+
+
+function restrict (req, res, next) {
+	if (!req.session.user) {
+		req.session.error = "Access denied";
+		res.redirect('login');
+	} else {
+		next();
+	}
+}
 
 function get_partials() {
     return {
@@ -37,8 +62,27 @@ app.get('/index.html', function (req, res) {
     });
 });
 
-app.get('/login.html', function (req, res) {
+app.get('/login', function (req, res) {
     res.render('login', {
+        locals: {},
+        partials: get_partials()
+    });
+});
+
+app.post('/login', function (req, res) {
+	if (req.body.username === 'gery' || req.body.password === 'gery') {
+		req.session.regenerate(function () {
+			req.session.user = 'gery';
+			res.redirect('lobby');
+		});
+	} else {
+		req.session.error = "Login failed.  Try again.";
+		res.redirect('login');
+	}
+});
+
+app.get('/lobby', restrict, function (req, res) {
+    res.render('lobby', {
         locals: {},
         partials: get_partials()
     });
