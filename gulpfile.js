b6c14c3f5a1d30fce1a6614683c6d4bd32a38c3b
Gery Debongnie <gery.debongnie@gmail.com>
2014-06-18 20:58:17 +0200

    various improvements to build system

--- gulpfile.js
+++ gulpfile.js
@@ -12,14 +12,15 @@ var gulp = require('gulp'),
     spawn = require('child_process').spawn,
     Datastore = require('nedb');
 
-var src_path = 'src',
-    assets_path = 'assets',
+//-----------------------------------------------------------------------------
+var assets_path = 'assets',
     build_path = '.tmp',
-    server_path = build_path + '/server',
-    client_path = build_path + '/client',
     static_path = build_path + '/static',
-    tests_path = build_path + '/tests';
-    
+    src_path = 'src',
+    build_src_path = build_path + '/src',
+    server_path = build_src_path + '/server',
+    client_path = build_src_path + '/client',
+    tests_path = build_path + '/tests';    
 
 //-----------------------------------------------------------------------------
 gulp.task('clean', function () {  
@@ -42,20 +43,12 @@ gulp.task('css', function() {
         .pipe(gulp.dest(static_path + '/css'));
 });
 
-gulp.task('server-es6-to-es5', function() {
-    return gulp.src([src_path + '/**/*.js', '!src/client{,/**}'])
-        .pipe(newer(server_path))
+gulp.task('es6-to-es5', function() {
+    return gulp.src([src_path + '/**/*.js'])
+        .pipe(newer(build_src_path))
         .pipe(es6transpiler())
             .on('error', function (error) { console.log(error.stack); this.emit('end'); })
-        .pipe(gulp.dest(server_path));
-});
-
-gulp.task('client-es6-to-es5', function() {
-    return gulp.src([src_path + '/client/**/*.js'])
-        .pipe(newer(client_path))
-        .pipe(es6transpiler())
-            .on('error', function (error) { console.log(error.stack); this.emit('end'); })
-        .pipe(gulp.dest(client_path));
+        .pipe(gulp.dest(build_src_path));
 });
 
 gulp.task('create-db', function () {
@@ -66,8 +59,9 @@ gulp.task('create-db', function () {
     users_db.insert({username: 'test', password: 'test'});
 });
 
-gulp.task('browserify', ['client-es6-to-es5'], function () {
-    browserify('./' + client_path + '/index.js').bundle({debug: true})
+gulp.task('browserify', ['es6-to-es5'], function () {
+    browserify('./' + client_path + '/index.js')
+        .bundle({debug: true})
         .pipe(source('lobby.js'))
         .pipe(gulp.dest(static_path + '/js'));
 });
@@ -79,7 +73,7 @@ gulp.task('prepare', function (cb) {
         'css',
         'create-db',
         'browserify',
-        'server-es6-to-es5',
+        'es6-to-es5',
         'tests-es6-to-es5',
     ];
     runSequence('clean', tasks, cb);
@@ -88,14 +82,15 @@ gulp.task('prepare', function (cb) {
 //-----------------------------------------------------------------------------
 gulp.task('develop', ['prepare'], function (done) {
     nodemon({
-        script: './.tmp/server/server/index.js',
-        watch: ['./.tmp/server']
+        script: server_path + '/index.js',
+        watch: [build_src_path, '!' + client_path],
     }).on('log', function (log) { console.log(log.colour); });
 
     gulp.watch('assets/styles/*.css', ['css']);
-    gulp.watch([src_path + '/**/*.js', '!src/client{,/**}'], ['server-es6-to-es5']);
-    gulp.watch([src_path + '/**/*.js', '!src/server{,/**}'], ['client-es6-to-es5']);
-    gulp.watch(['tests/**/*.js'], ['run-tests']);
+    gulp.watch([src_path + '/**/*.js'], ['es6-to-es5']);
+    gulp.watch(['tests/**/*.js'], ['tests-es6-to-es5']);
+    gulp.watch(['./.tmp/src/**/*.js'], ['run-tests']);
+    gulp.watch(['./.tmp/tests/**/*.js'], ['run-tests']);
 });
 
 gulp.task('default', ['develop']);
@@ -110,80 +105,16 @@ gulp.task('tests-es6-to-es5', function() {
         .pipe(gulp.dest(tests_path));
 });
 
-gulp.task('run-tests', ['tests-es6-to-es5'], function (cb) {
+gulp.task('run-tests', function (cb) {
     var tests = spawn('mocha', [tests_path, '--recursive','-R','spec'], {stdio: 'inherit'});
     tests.on('close', cb);
 });
 
-    // rename = require('gulp-rename'),
-    // browserify = require('gulp-browserify'), // BLACKLISTED
     // uglify = require('gulp-uglify'),
-    // spawn = require('child_process').spawn;
 
-
-// gulp.task('css', function() {
-//     return gulp.src('src/client/css/*.css')
-//         .pipe(gulp.dest('static/css/'));
-// });
-
-// gulp.task('html', function() {
-//     return gulp.src('src/client/html/*.html')
-//         .pipe(gulp.dest('static/'));
-// });
-
-// gulp.task('js', function() {
-//     return gulp.src('src/client/js/index.js')
-//         .pipe(rename({basename: 'netrunner'}))
-//         .pipe(browserify({
-//             insertGlobals : true,
-//             debug : true,
-//         }))
-//         .pipe(traceur({sourceMap: true, experimental: true}))
-//         .pipe(gulp.dest('static/js/'));
-// });
-
-// gulp.task('traceur_runtime', function () {
-//     return gulp.src('node_modules/traceur/bin/traceur-runtime.js')
-//         // .pipe(uglify())
-//         .pipe(gulp.dest('static/js/'));
-// });
-
-// //-----------------------------------------------------------------------------
-// gulp.task('test', function (cb) {
-//     var tests = spawn('mocha', ['tests', '--recursive','-R','spec'], {stdio: 'inherit'});
-//     tests.on('close', cb);
-
-//     tests.on('error', function () {
-//         console.log('Failure...');
-//         cb();
-//     });
-
-// });
-
-// //-----------------------------------------------------------------------------
-// gulp.task('prepare', function (cb) {
-//     runSequence('clean', ['css','js', 'html', 'traceur_runtime'], cb);
-// });
-
-// //-----------------------------------------------------------------------------
-// gulp.task('develop', ['prepare'], function (done) {
-//     nodemon({
-//         script: './src/server/index.js',
-//         watch: ['./src/server', './src/common']
-//     }).on('log', function (log) { console.log(log.colour); });
-
-//     gulp.watch('src/client/css/*.css', ['css']);
-//     gulp.watch('src/client/js/*.js', ['js']);
-//     gulp.watch('src/client/html/*.html', ['html']);
-
-//     gulp.watch('src/**/*.js', ['test']);
-//     gulp.watch('tests/**/*.js', ['test']);
-// });
 
 // //-----------------------------------------------------------------------------
 // gulp.task('serve', ['prepare'], function () {
 //     require('./src/server');
 // });
 
-// //-----------------------------------------------------------------------------
-// gulp.task('default', ['develop']);
