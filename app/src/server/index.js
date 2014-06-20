b59515e0a2398935ea5dbf629666d72518a1dcee
Gery Debongnie <gery.debongnie@gmail.com>
2014-06-20 11:19:12 +0200

    remove useless comments

--- app/src/server/index.js
+++ app/src/server/index.js
@@ -2,14 +2,11 @@
 'use strict';
 
 let Server = require('./server.js'),
-	port = process.env.PORT || 8080,
 	logger = require('../logger');
 
-// console.log(__dirname);
+let	port = process.env.PORT || 8080,
+	paths = require(process.env.CONFIG_FOLDER + 'paths.json').build;
 
-let paths = require(process.env.CONFIG_FOLDER + 'paths.json').build;
-// console.log(process.argv);
-// console.log(process.env.CONFIG_FOLDER);
 //-----------------------------------------------------------------------------
 new Server(port, paths, {logger:logger});
 
