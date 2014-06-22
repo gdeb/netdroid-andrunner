0c903a80dbaad45679a2bb9378e827ae341bc904
Gery Debongnie <gery.debongnie@gmail.com>
2014-06-22 14:29:47 +0200

    properly use ' instead of "

--- app/src/client/login.js
+++ app/src/client/login.js
@@ -3,7 +3,7 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', function () {
-	let passwords = document.getElementsByClassName("password");
+	let passwords = document.getElementsByClassName('password');
 	for (let i = 0; i < passwords.length; i++) {
 		passwords[i].setAttribute('type', 'password');
 	}
