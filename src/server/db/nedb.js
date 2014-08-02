e36c847f5ab919438a00abff89ed182e172bf8e2
Gery Debongnie <gery.debongnie@gmail.com>
2014-08-02 12:03:50 +0200

    work on db

--- src/server/db/nedb.js
+++ src/server/db/nedb.js
@@ -1,78 +1,47 @@
 /*jslint node: true */
 'use strict';
 
-
-module.exports = function (folder, logger) {
-
-	return {
-
-	}
-};
-
-
 let Datastore = require('nedb'),
-	logger = require('../logger.js'),
 	fs = require('fs');
 
-//-----------------------------------------------------------------------------
-let db;
+module.exports = function (folder, logger) {
+	let db = {},
+		collections = {};
 
-let	initial_users = [
-	{username: "gery", password: "gery"},
-	{username: "g", password: "g"},
-	{username: "demo", password: "pw"},
-];
-
-//-----------------------------------------------------------------------------
-function initialize(folder) {
-	let db_existed = fs.existsSync(folder + '/users.db');
-	db = new Datastore({filename: folder + '/users.db', autoload: true});
-	if (!db_existed) {
-		for (let user of initial_users) {
-			add_user(user);
-		}		
-	}
-}
-
-function validate_user (user) {
-	return true;
-}
-
-function add_user (user) {
-	if (validate_user(user)) 
-		db.insert(user);
-	else
-		throw new Error('Invalid user');
-}
-
-function find_user (name, password, callback) {
-	db.find({
-		username: name, 
-		password: password
-	}, callback);
-}
-
-function update_password (name, old_pw, new_pw, cb) {
-	let user_info = {
-		username: name,
-		password: old_pw,
+	db.load = function (name) {
+		let filename = `${folder}/${name}.db`,
+			created = !fs.existsSync(filename);
+		collections[name] = new Datastore({
+			filename: filename,
+			autoload: true
+		});
+		return created;
 	};
-	db.find(user_info, function (err, users) {
-		if (users.length) {
-			db.update(user_info, {$set: {password: new_pw}}, function (err) {
-				return cb(null, "success");
-			});
+
+	db.insert = function (name, document) {
+		if (name in collections) {
+			collections[name].insert(document);
 		} else {
-			cb('wrong password');
+			throw new Error(`Collection ${name} does not exist.`);			
 		}
-	});
+	};
 
-}
+	db.find = function (name, info, callback) {
+		if (name in collections) {
+			collections[name].find(info, callback);
+		} else {
+			throw new Error(`Collection ${name} does not exist.`);			
+		}
+	};
 
-//-----------------------------------------------------------------------------
-module.exports = {
-	init: initialize,
-	add: add_user,
-	find: find_user,
-	update_password: update_password,
+	db.update = function (name, update, callback) {
+		if (name in collections) {
+			collections[name].update(update, callback);
+		} else {
+			throw new Error(`Collection ${name} does not exist.`);			
+		}
+	};
+
+	return db;
 };
+
