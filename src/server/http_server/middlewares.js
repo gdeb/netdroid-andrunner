f5e617ec64ebcb69285aff1dbcedfb1f3a924a66
Gery Debongnie <gery.debongnie@gmail.com>
2014-08-06 09:57:13 +0200

    improve http_logger middleware

--- src/server/http_server/middlewares.js
+++ src/server/http_server/middlewares.js
@@ -11,23 +11,21 @@ module.exports.http_logger = function (logger) {
 	return function (req, res, next) {
 		const start = process.hrtime();
 
-	   	function logRequest(){
-	      	let diff = process.hrtime(start),
-	      		request_time = (diff[0] * 1e3 + diff[1] * 1e-6).toFixed(3),
-	      		http = req.httpVersionMajor + '.' + req.httpVersionMinor,
-	      		url = req.originalUrl || req.url,
-	      		code = res.statusCode,
-	      		status = (code === 404) ? String(code).red : code;
-
-	      	logger.info([
-	      		req.ip,
-	      		`${req.method} ${req.url} (HTTP/${http})`,
-	      		`status: ${status}`,
-	      		`${request_time} ms`,
-	      	].join(', '));
-	    }
-	    res.once('finish', logRequest);
+	    res.once('finish', function () {
+	    	logger.info(format_log(req, res, start));
+	    });
 		next();
 	};
 };
 
+function format_log(req, res, start){
+  	let diff = process.hrtime(start),
+  		request_time = (diff[0] * 1e3 + diff[1] * 1e-6).toFixed(3),
+  		http = req.httpVersionMajor + '.' + req.httpVersionMinor,
+  		url = req.originalUrl || req.url,
+  		code = res.statusCode,
+  		status = (code === 404) ? String(code).red : code;
+
+  	return `${req.ip}, ${req.method} ${req.url} (HTTP/${http})` +
+  		   `status: ${status}, ${request_time} ms`;
+}
