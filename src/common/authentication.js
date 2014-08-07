679e0b06b8af9f2a46913d64891a979d4f0700f1
Gery Debongnie <gery.debongnie@gmail.com>
2014-08-07 09:45:15 +0200

    improve descriptions of roles/access levels

--- src/common/authentication.js
+++ src/common/authentication.js
@@ -1,20 +1,35 @@
 /*jslint node: true */
 'use strict';
 
-const ROLES = {
-    public: 1,
-    user: 2,
-    admin: 4,
+let roles = {
+	public: 1,
+	user: 2,
+	admin: 4,
 };
 
-const ACCESS_LEVELS = {
-	public: ROLES.public | ROLES.user | ROLES.admin,
-    anon: ROLES.public,
-    user: ROLES.user | ROLES.admin,
-    admin: ROLES.admin,
+let access_levels = {
+	public: roles.public | roles.user | roles.admin,
+	anon : roles.public,
+	user: roles.user | roles.admin,
+	admin: roles.admin,
 };
+
+let roles_descr = {};
+roles_descr[roles.public] = "public";
+roles_descr[roles.user] = "user";
+roles_descr[roles.admin] = "admin";
+
+
+let access_levels_descr = {};
+access_levels_descr[access_levels.public] = "public";
+access_levels_descr[access_levels.anon] = "anon";
+access_levels_descr[access_levels.user] = "user";
+access_levels_descr[access_levels.admin] = "admin";
 
 module.exports = {
-	user_roles: ROLES,
-	access_levels: ACCESS_LEVELS,
+	user_roles: roles,
+	roles_description: roles_descr,
+	access_levels: access_levels,
+	access_levels_description: access_levels_descr,
 };
+
