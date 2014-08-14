970c07efd4fe40cebe6f4e4e1620d260dabf7195
Gery Debongnie <gery.debongnie@gmail.com>
2014-08-14 11:28:15 +0200

    solidify websocket server message handling

--- src/server/websocket_server/server.js
+++ src/server/websocket_server/server.js
@@ -15,8 +15,10 @@ module.exports = function (logger, session) {
 			logger.info("WebSocket Server started on port " + port + ".");
 		},
 		add_route (route) {
-			if (!('url' in route) || !('controller' in route))
+			if (!('url' in route) || !('controller' in route)) {
+				logger.error('Attempt to add invalid route:', JSON.stringify(route));
 				throw new Error('Invalid route:');
+			}
 			routes[route.url] = route.controller;
 		},
 	};
@@ -25,21 +27,32 @@ module.exports = function (logger, session) {
 		cookie_parser(socket.upgradeReq, null, function (err) {
 			var session_id = socket.upgradeReq.signedCookies['connect.sid'];
 			session_store.get(session_id, function (err, session) {
-				session.websocket = socket;
-				socket.on('message', msg => dispatch(msg, session));
+				if ('user' in session) {
+					logger.info(`New connection from user ${session.user}`);
+					session.websocket = socket;
+					socket.on('message', msg => dispatch(msg, session));
+				} else {
+					logger.info('Connection attempt from unlogged user. Connection closed');
+					socket.close();
+				}
 			});
 		});	
 	}
 
 	function dispatch (msg, session) {
-		logger.debug(msg);
-		logger.debug(msg.route);
-		let json_msg = JSON.parse(msg);
-		logger.info('Received message on ws');
+		logger.info(`Received message from ${session.user} : ${msg}`);
+		var json_msg;
+		try {
+			json_msg = JSON.parse(msg);
+		} 
+		catch (e) {
+			logger.error('Invalid message (should be a JSON parsable string)');
+			return;
+		}
 		if (json_msg.route in routes) {
 			routes[json_msg.route](json_msg, session);
 		} else {
-			logger.warn(`Message ${msg} was not dispatched to a controller`);
+			logger.warn(`No valid route for message ${msg}`);
 		}
 	}
 
