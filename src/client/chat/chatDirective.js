a52999da0355cec1607707e76d06fde4efd5648c
Gery Debongnie <gery.debongnie@gmail.com>
2014-08-14 09:29:20 +0200

    prep work for chat feature

--- src/client/chat/chatDirective.js
+++ src/client/chat/chatDirective.js
@@ -2,7 +2,16 @@
 angular.module('chat').directive('anrChat', function () {
     return {
         restrict: 'E',
+        scope: {},
         templateUrl: 'templates/partial-chat.html',
+        link: function (scope, element) {
+        	scope.test = "nrst";
+        	scope.chatmsg = "ab";
+        	scope.testf = function () {
+				console.log('hello nrst', scope.chatmsg);
+				scope.chatmsg = "";
+        	}; 
+        },
     };    
 });
 
